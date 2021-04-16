abdc7368f4132b8e14881b722762b9d2bd6a41be
Adeline MOUTOU <adeline.moutou@ens.uvsq.fr>
2021-04-16 10:43:56 +0200

    Projet2

--- test.py
+++ test.py
@@ -1,42 +1,22 @@
-# assigning key directions
-def goup():
-    if head.direction != "down":
-        head.direction = "up"
-  
-  
-def godown():
-    if head.direction != "up":
-        head.direction = "down"
-  
-  
-def goleft():
-    if head.direction != "right":
-        head.direction = "left"
-  
-  
-def goright():
-    if head.direction != "left":
-        head.direction = "right"
-  
-  
-def move():
-    if head.direction == "up":
-        y = head.ycor()
-        head.sety(y+20)
-    if head.direction == "down":
-        y = head.ycor()
-        head.sety(y-20)
-    if head.direction == "left":
-        x = head.xcor()
-        head.setx(x-20)
-    if head.direction == "right":
-        x = head.xcor()
-        head.setx(x+20)
-  
-  
-wn.listen()
-wn.onkeypress(goup, "w")
-wn.onkeypress(godown, "s")
-wn.onkeypress(goleft, "a")
-wn.onkeypress(goright, "d")
-           
+if direction  == 'gauche':
+        Serpent[0][0]  = Serpent[0][0] - dx
+        if Serpent[0][0] < 0:
+            Serpent[0][0] = 493
+    elif direction  == 'droite':
+        Serpent[0][0]  = Serpent[0][0] + dx
+        if Serpent[0][0] > 493:
+            Serpent[0][0] = 0
+    elif direction  == 'haut':
+        Serpent[0][1]  = Serpent[0][1] - dy
+        if Serpent[0][1] < 0:
+            Serpent[0][1] = 493
+    elif direction  == 'bas':
+        Serpent[0][1]  = Serpent[0][1] + dy
+        if Serpent[0][1] > 493:
+            Serpent[0][1] = 0
+    can.create_oval(Serpent[0][0], Serpent[0][1], Serpent[0][0]+10, Serpent[0][1]+10,outline='green', fill='blue')
+    test()
+    test()
+   
+    if flag != 0:
+        fen.after(60, move)
