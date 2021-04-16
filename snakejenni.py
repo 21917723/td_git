e36fefdd961104aa67dcb978474a3b07bb2b9ef1
Adeline MOUTOU <adeline.moutou@ens.uvsq.fr>
2021-04-16 11:03:03 +0200

    projet2

--- snakejenni.py
+++ snakejenni.py
@@ -1,2 +1,4 @@
 #a fonction move ou after avec des niveaux (facil, moyen, difficil)
 #creation de trois boutons 
+#POUR LES AUTRES NIVEAU DE JEU FAIRE APPARAITRE DES MURS DANS L'ESPACE 
+#POUVOIR CHOISIR SON NIVEAU AU DEBUT DU PROGRAMME 
